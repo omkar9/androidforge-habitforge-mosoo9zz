cc75b8e69b970b3258023817dfeffed96f6e5fb0
omkar9 <omkar946@gmail.com>
2026-05-05 19:20:25 +0530

    feat: add Converters.kt

---
+++ app/src/main/java/com/androidforge/habitforge/data/local/converter/Converters.kt
@@ -0,0 +1,15 @@
+package com.androidforge.habitforge.data.local.converter
+
+import androidx.room.TypeConverter
+import java.time.LocalDate
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): LocalDate? {
+        return value?.let { LocalDate.ofEpochDay(it) }
+    }
+
+    @TypeConverter\    fun dateToTimestamp(date: LocalDate?): Long? {
+        return date?.toEpochDay()
+    }
+}
