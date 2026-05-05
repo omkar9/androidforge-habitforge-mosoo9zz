221cffd36b4919a535ca72c4bc2f66df157457a3
omkar9 <omkar946@gmail.com>
2026-05-05 19:21:30 +0530

    fix: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,7 @@
 plugins {
     id("com.android.application") version "8.3.2" apply false
-    id("com.android.library") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.23" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
 }
-
-buildscript {
-    dependencies {
-        // No extra dependencies needed here, versions are handled by libs.versions.toml
-    }
-}
-
-// No subprojects block or allprojects block needed for a single-module app.
-// Dependencies are managed via libs.versions.toml
