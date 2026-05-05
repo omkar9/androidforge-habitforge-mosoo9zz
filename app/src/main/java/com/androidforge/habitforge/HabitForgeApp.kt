c48e393bc23728f1bd20391e7a244ce6e3404f54
omkar9 <omkar946@gmail.com>
2026-05-05 19:21:59 +0530

    fix: update HabitForgeApp.kt

--- app/src/main/java/com/androidforge/habitforge/HabitForgeApp.kt
+++ app/src/main/java/com/androidforge/habitforge/HabitForgeApp.kt
@@ -1,33 +1,7 @@
 package com.androidforge.habitforge
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
-import com.androidforge.habitforge.core.util.AdManager
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
-class HabitForgeApp : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    @Inject
-    lateinit var adManager: AdManager
-
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-        adManager.initializeMobileAds()
-        adManager.loadInterstitialAd() // Pre-load interstitial ad on app start
-    }
-}
+class HabitForgeApp : Application()
