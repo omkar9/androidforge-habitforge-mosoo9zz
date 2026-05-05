1cd89c5f1fc6c9417e949b20b0cd1e62a4d3ce2d
omkar9 <omkar946@gmail.com>
2026-05-05 19:20:37 +0530

    feat: add AppModule.kt

---
+++ app/src/main/java/com/androidforge/habitforge/core/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.androidforge.habitforge.core.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApplicationContext(@ApplicationContext context: Context): Context = context
+
+    @Provides
+    @Singleton
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
