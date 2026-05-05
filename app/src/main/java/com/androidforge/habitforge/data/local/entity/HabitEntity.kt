a29395948ae0d00da134914fbbb9cfb8abfe24e6
omkar9 <omkar946@gmail.com>
2026-05-05 19:20:04 +0530

    feat: add HabitEntity.kt

---
+++ app/src/main/java/com/androidforge/habitforge/data/local/entity/HabitEntity.kt
@@ -0,0 +1,12 @@
+package com.androidforge.habitforge.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "habits")
+data class HabitEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val description: String,
+    val createdAt: Long
+)
