0d6c16c1de94b1ee4ba8fc3e8195542e46a540d9
omkar9 <omkar946@gmail.com>
2026-05-05 19:18:45 +0530

    feat: add Color.kt

---
+++ app/src/main/java/com/androidforge/habitforge/presentation/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.androidforge.habitforge.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF0A6E82)
+val PrimaryVariant = Color(0xFF0D92A8)
+val Secondary = Color(0xFFE0B46B)
+val Background = Color(0xFF121212)
+val Surface = Color(0xFF1E1E1E)
+val SurfaceVariant = Color(0xFF2C2C2C)
+val OnPrimary = Color(0xFFFFFFFF)
+val OnBackground = Color(0xFFE0E0E0)
+val OnSurface = Color(0xFFE0E0E0)
+val OnSurfaceVariant = Color(0xFFB0B0B0)
+val Success = Color(0xFF5CB85C)
+val Error = Color(0xFFD9534F)
+val Warning = Color(0xFFF0AD4E)
+
+val DarkPrimary = Primary
+val DarkPrimaryVariant = PrimaryVariant
+val DarkSecondary = Secondary
+val DarkBackground = Background
+val DarkSurface = Surface
+val DarkSurfaceVariant = SurfaceVariant
+val DarkOnPrimary = OnPrimary
+val DarkOnBackground = OnBackground
+val DarkOnSurface = OnSurface
+val DarkOnSurfaceVariant = OnSurfaceVariant
+val DarkSuccess = Success
+val DarkError = Error
+val DarkWarning = Warning
