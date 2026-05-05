e142e2d7f75cea28e8deb8464cd19c8c1f7c936f
omkar9 <omkar946@gmail.com>
2026-05-05 19:20:52 +0530

    feat: add MainActivity.kt

---
+++ app/src/main/java/com/androidforge/habitforge/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.androidforge.habitforge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.androidforge.habitforge.presentation.navigation.HabitForgeNavHost
+import com.androidforge.habitforge.presentation.theme.HabitForgeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            HabitForgeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    HabitForgeNavHost(navController = navController)
+                }
+            }
+        }
+    }
+}
