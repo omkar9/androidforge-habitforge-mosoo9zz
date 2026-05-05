47b441c6197a1d4a9cb7a0a352d86fa8f82fc32b
omkar9 <omkar946@gmail.com>
2026-05-05 19:19:44 +0530

    feat: add MarkHabitCompletedUseCase.kt

---
+++ app/src/main/java/com/androidforge/habitforge/domain/usecase/MarkHabitCompletedUseCase.kt
@@ -0,0 +1,14 @@
+package com.androidforge.habitforge.domain.usecase
+
+import com.androidforge.habitforge.core.util.DateUtils
+import com.androidforge.habitforge.core.util.Result
+import com.androidforge.habitforge.domain.model.CompletionType
+import com.androidforge.habitforge.domain.repository.HabitRepository
+import javax.inject.Inject
+
+class MarkHabitCompletedUseCase @Inject constructor(private val repository: HabitRepository) {
+    suspend operator fun invoke(habitId: Long): Result<Unit> {
+        val today = DateUtils.today()
+        return repository.markHabitCompletion(habitId, today, CompletionType.COMPLETED)
+    }
+}
